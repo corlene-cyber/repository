6fe05fcf0b088d3d032baaf580a02546fef34abb
corlene-cyber <cgreenwood@eggs.school.nz>
2020-06-26 17:07:50 +1200

    Update 02_Help_GUI_part_1.py

--- 02_Help_GUI_part_1.py
+++ 02_Help_GUI_part_1.py
@@ -3,8 +3,14 @@ import random
 
 
 class Convertor:
-    def __init__(self, parent):
-        print("hello world")
+    def __init__(self):
+       
+       #Formatting variables...
+       background_color = "light blue"
+       
+       #Convertor Main Screen GUI...
+       self.converter_frame = Frame (width=600, height=600, bg=background_color)
+       self.converter_frame.grid()
 
 
 # main routine
